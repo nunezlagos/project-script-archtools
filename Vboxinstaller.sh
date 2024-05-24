45b5f2d90fd36f1b7825e2e0ef17957ffeee8eb4
Mauricio Nuñez Lagos (Mauri) <mauricio@nunezlagos.cl>
2024-05-23 20:58:09 -0400

    Update Vboxinstaller.sh

--- Vboxinstaller.sh
+++ Vboxinstaller.sh
@@ -55,8 +55,8 @@ mkdir -p ~/.config/alacritty
 
 # Copy default configurations
 echo "Copying default configurations..."
-cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
-cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
+cp /archtools/bspwmrc ~/.config/bspwm/
+cp /archtools/sxhkdrc ~/.config/sxhkd/
 cp /etc/xdg/picom.conf ~/.config/picom/
 cp /etc/polybar/config.ini ~/.config/polybar/
 cp /etc/dunst/dunstrc ~/.config/dunst/
@@ -64,53 +64,6 @@ cp /etc/dunst/dunstrc ~/.config/dunst/
 # Make the bspwmrc file executable
 echo "Making the bspwmrc file executable..."
 chmod +x ~/.config/bspwm/bspwmrc
-
-# Configure Alacritty to use Zsh
-echo "Configuring Alacritty to use Zsh..."
-cat <<EOL > ~/.config/alacritty/alacritty.yml
-shell:
-  program: /usr/bin/zsh
-  args:
-    - --login
-EOL
-
-# Install Oh-My-Zsh
-echo "Installing Oh-My-Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Configure Oh-My-Zsh theme to 'agnoster'
-echo "Configuring Oh-My-Zsh theme to 'agnoster'..."
-sed -i 's/ZSH_THEME=".*"/ZSH_THEME="agnoster"/' ~/.zshrc
-
-# Update sxhkdrc with necessary changes
-echo "Updating sxhkdrc..."
-cat <<EOL > ~/.config/sxhkd/sxhkdrc
-# terminal emulator
-super + Return
-	alacritty
-
-# Open Firefox
-super + b
-	firefox
-EOL
-
-# Configure bspwmrc
-echo "Configuring bspwmrc..."
-cat <<EOL > ~/.config/bspwm/bspwmrc
-#!/bin/bash
-
-# Start sxhkd, dunst, and polybar
-sxhkd &
-dunst &
-polybar &
-
-# Remove specific rules
-bspc rule -a Gimp desktop='^8' state=floating follow=on
-bspc rule -a Chromium desktop='^2'
-bspc rule -a mplayer2 state=floating
-bspc rule -a Kupfer.py focus=on
-bspc rule -a Screenkey manage=off
-EOL
 
 echo "Configuration complete. Please restart your terminal or run 'exec zsh' to apply the changes."
 
