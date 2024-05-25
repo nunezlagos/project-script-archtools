035cce68fd587490995f0212d617648c24e7a635
Mauricio Nuñez Lagos (Mauri) <mauricio@nunezlagos.cl>
2024-05-25 00:11:28 -0400

    Add files via upload

--- Config/polybar/launch.sh
+++ Config/polybar/launch.sh
@@ -11,15 +11,22 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 ## Launch
 
 ## Left bar
-polybar log -c ~/.config/polybar/current.ini &
+
 polybar secondary -c ~/.config/polybar/current.ini &
-polybar terciary -c ~/.config/polybar/current.ini &
-polybar quaternary -c ~/.config/polybar/current.ini &
-polybar quinary -c ~/.config/polybar/current.ini &
+#polybar terciary -c ~/.config/polybar/current.ini &
+
+#polybar quinary -c ~/.config/polybar/current.ini &
 
 ## Right bar
 polybar top -c ~/.config/polybar/current.ini &
-polybar primary -c ~/.config/polybar/current.ini &
+polybar quaternary -c ~/.config/polybar/current.ini &
+polybar music -c ~/.config/polybar/current.ini &
 
 ## Center bar
 polybar primary -c ~/.config/polybar/workspace.ini &
+
+
+## Bottom bar
+polybar bottom -c ~/.config/polybar/workspace.ini &
+polybar log -c ~/.config/polybar/current.ini &
+#bOKITA LO MAS GRANDE
