8bf44b1c644a320e99f6bffe1e27a4f5455074e5
Mauricio Nuñez Lagos (Mauri) <mauricio@nunezlagos.cl>
2024-05-25 20:48:57 -0400

    Update launch.sh

--- Config/polybar/launch.sh
+++ Config/polybar/launch.sh
@@ -10,23 +10,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 ## Launch
 
-## Left bar
 
-polybar secondary -c ~/.config/polybar/current.ini &
-#polybar terciary -c ~/.config/polybar/current.ini &
+polybar primary -c ~/.config/archtools/polybar/workspace.ini &
+polybar main_primary -c ~/.config/archtools/polybar/current.ini &
+polybar main_secondary -c ~/.config/archtools/polybar/current.ini &
 
-#polybar quinary -c ~/.config/polybar/current.ini &
-
-## Right bar
-polybar top -c ~/.config/polybar/current.ini &
-polybar quaternary -c ~/.config/polybar/current.ini &
-polybar music -c ~/.config/polybar/current.ini &
-
-## Center bar
-polybar primary -c ~/.config/polybar/workspace.ini &
-
-
-## Bottom bar
-polybar bottom -c ~/.config/polybar/workspace.ini &
-polybar log -c ~/.config/polybar/current.ini &
-#bOKITA LO MAS GRANDE
